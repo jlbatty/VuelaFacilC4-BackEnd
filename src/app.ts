a2f55071352be779ab661000fe7079ab23f4938d
Johnatan Andrés Arango R <ingyotis@gmail.com>
2021-11-09 19:33:58 -0500

    Algunas líneas añadidas al app. Para parsear el body de los requests

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
+//con estas líneas de código usamos el parser JSON de express para obtener el Body de la request.
+app.use(express.json())
+app.use(express.urlencoded( {extended: true} ))
+
 app.get('/', (req, res) => {
     res.send('prueba del servidor');
 });
